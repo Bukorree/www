6e3338be602eaf49e968cf8f0d7481b04ea0970e
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-10-30 17:18:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <body>
         <img src="cat1.png"><br>
         <meta charset="UTF-8">
-        <h1>name :Bukorree  Madthing</h1>
-        <h2>Nickname: film</h2>
+        <h1>Name :Bukorree  Madthing</h1>
+        <h2>Nickname: Film</h2>
         <br>
         <a href="http://www.it.kmitl.ac.th">Link to IT KMITL Website</a>
         <a href="https://github.com/whitewolf5432/Lab_ITF_2563"><img src="cat.jpg"></a>

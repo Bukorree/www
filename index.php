11f5b8e53056bb65d2b0a4af65e9afecffe3bf28
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-10-30 17:40:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@
         <h3>This is a cat</h3>
         <br><iframe width="893" height="502" src="https://www.youtube.com/embed/rvAL1pdRLkI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <h1><a href="https://www.youtube.com/watch?v=rvAL1pdRLkI">Link to This music</a></h1>
-        <p>ณ คลีนิครักษาคนป่วย เวลา 2 ทุ่ม
-            คนป่วย : คุณหมอ อาการผมเป็นไงบ้างครับ
-            หมอ : เสียใจด้วย…หมอคิดว่า คุณคงจะอยู่ได้อีกไม่นาน
-            คนป่วย : ผมเป็นโรคร้ายแรงอะไรเหรอครับหมอ !!!
-            หมอ : คลีนิคปิด 2 ทุ่มครึ่ง</p>
+        <p>ณ คลีนิครักษาคนป่วย เวลา 2 ทุ่ม</p>
+           <p> คนป่วย : คุณหมอ อาการผมเป็นไงบ้างครับ</p>
+           <p>หมอ : เสียใจด้วย…หมอคิดว่า คุณคงจะอยู่ได้อีกไม่นาน</p>
+           <p> คนป่วย : ผมเป็นโรคร้ายแรงอะไรเหรอครับหมอ !!!</p>
+           <p> หมอ : คลีนิคปิด 2 ทุ่มครึ่ง</p>
     </body>
 </html>

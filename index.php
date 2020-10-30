1d9e084dd33465919d47eda1a4da6a0ba6fa4e54
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-10-30 17:32:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,6 @@
         <a href="https://github.com/whitewolf5432/Lab_ITF_2563"></a><br>
         <img src="01.jpg">
         <br><iframe width="893" height="502" src="https://www.youtube.com/embed/rvAL1pdRLkI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <a href="https://www.youtube.com/embed/rvAL1pdRLkI">Link to This music</a>
+        <h1><a href="https://www.youtube.com/embed/rvAL1pdRLkI">Link to This music</a></h1>
     </body>
 </html>

8be0af94045efc3848ba10e847bcb1e589d3e29c
Bukorree <73693612+Bukorree@users.noreply.github.com>
2020-10-30 17:28:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
         <h1>Name : Bukorree  Madthing</h1>
         <h2>Nickname: Film</h2>
         <br>
-        <a href="http://www.it.kmitl.ac.th">Link to IT KMITL Website</a>
-        <a href="https://github.com/whitewolf5432/Lab_ITF_2563"></a>
+        <a href="http://www.it.kmitl.ac.th">Link to IT KMITL Website</a><br>
+        <a href="https://github.com/whitewolf5432/Lab_ITF_2563"></a><br>
         <img src="cat.jpg">
-        <iframe width="893" height="502" src="https://www.youtube.com/embed/rvAL1pdRLkI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <br><iframe width="893" height="502" src="https://www.youtube.com/embed/rvAL1pdRLkI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <a href="http://www.it.kmitl.ac.th">Link to IT KMITL Website</a>
     </body>
 </html>
